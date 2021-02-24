af353c2731784fa842a2212416ac654e9b5dd843
IgnatioFerreira <ignatio.ferreira@gmail.com>
2021-02-24 11:18:32 +0200

    init

--- prg381.java
+++ prg381.java
@@ -2,6 +2,6 @@ public class prg381{
     public static void main(String[] args) {
         System.out.println("my first git file on java");
         System.out.println("The secon line");
-        ////////comment
+        //////////comment
     }
 }
